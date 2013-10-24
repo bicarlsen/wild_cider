4354fd1d9e3c3d177cc42e662af7713756166398
Brian Carlsen <carlsen.bri@gmail.com>
2013-10-24 01:26:10 -0600

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WildCider::Application.config.secret_key_base = 'd73343fec64fdca0ec2a242090402bfda7e2ed2d97e933eba42a5863691bd1d31994a46d9db4d127ad3ceb9ecfd1804e11d350715fe6b82d46c9e38e900a1979'
